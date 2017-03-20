4917188fe446ae90e39924426c2aca0dd2eaa5bf
wangRan <307209868@qq.com>
2017-03-20 19:31:38 +0800

    add README

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     entry: require('./webpack-config/entry.config'),
     output: require('./webpack-config/output.config'),
-    module: require('./webpack-config/module.product.config'),
-    resovle: require('./webpack-config/resolve.config'),
-    plugins: require('./webpack-config/plugins.product.config'),
+    module: require('./webpack-config/module.dev.config'),
+    resolve: require('./webpack-config/resolve.config'),
+    plugins: require('./webpack-config/plugins.dev.config'),
     externals: require('./webpack-config/externals.config')
 }
